c40da976408982980c981a60f378b495da9f9409
Learning <karar.khaled1993@gmail.com>
2021-12-14 17:05:16 +0200

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Karrar Almukhtar on 14.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
